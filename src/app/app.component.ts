2d60e7aace6a3f8cdf424b386fe522d6570b7a64
Manuelb1990 <manuell@bluewin.ch>
2021-12-04 19:04:09 +0100

    add animation Img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'modul11angular';
+export class AppComponent implements OnInit {
+  /*title = 'modul11angular';*/
+  images = ['picture1.jpg', 'picture2.jpg', 'picture3.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  //wird bei start ausgefuert. 
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      
+      setTimeout(()=>{
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+
+  }
 }
